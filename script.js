b01b11711f86c2b7aad1c6d28e15c28ed242d794
Theafs <theafs07@icloud.com>
2025-02-23 23:59:10 +0100

    variabel og ball

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
+//variabler
+var DIRECTION = {
+    IDLE: 0,
+    UP: 1,
+    DOWN: 2,
+    LEFT: 3,
+    RIGHT: 4,
+};
 
- 
+var rounds = [ 5, 5, 3, 3, 2];
+var colors = ['#1abc9c', '#2ecc71', '#3498db', '#8c52ff', '#9b59b6']
+
+//ballen
+var Ball = {
+    new: function (incrementedSpeed) {
+        return {
+            width: 18,
+            height: 18,
+            x: (this.canvas.width / 2) - 9,
+            y: (this.canvas.height /2) -9,
+            moveX: DIRECTION.IDLE,
+            moveY: DIRECTION.IDLE,
+            speed: incrementedSpeed || 7
+        };
+    }
+} ;
