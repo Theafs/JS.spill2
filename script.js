32f0de9c99b9b85c6efb8ceea35b6cf86a0dda7d
Theafs <theafs07@icloud.com>
2025-02-24 23:45:28 +0100

    paddel

--- script.js
+++ script.js
@@ -4,6 +4,31 @@ const canvas = document.getElementById('pong');
 const kontekst = canvas.getContext('2d');
 const start = document.getElementById('startButton');
 
+//paddelstørelse 
+const paddleBredde = 10, paddleHøyde = 100;
+
+//Spiller sin paddel
+const spiller = {
+    x: 0,
+    y: canvas.height / 2 - paddleHøyde / 2,
+    width: paddleBredde,
+    height: paddleHøyde,
+    color: 'white',
+    dy: 10,
+    score: 0
+};
+
+//maskinen sin paddel
+const datamaskin = {
+    x: canvas.width - paddleBredde,
+    y: canvas.height / 2 - paddleHøyde / 2,
+    width: paddleBredde,
+    height: paddleHøyde,
+    color: 'white',
+    dy: 5,
+    score: 0
+};
+
 //starter spillet
 start.addEventListener('click', () => {
     start.style.display = 'none';
